9b964e5dfdf6b4178d80cea35ebe2afde381a738
Vitalii <vitaliy.poltavets@developex.net>
2020-07-20 22:02:27 +0300

    Add support only for light interface style

--- Sleeper/Sleeper/AppDelegate.swift
+++ Sleeper/Sleeper/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
+        window!.overrideUserInterfaceStyle = .light
         appCoordinator = AppCoordinator(window: window!)
         appCoordinator.makeRootAccordingToUser()
         
