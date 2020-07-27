1540e3e29e70326daff07a0c2a05fe044f06d9fb
Vitalii <vitaliy.poltavets@developex.net>
2020-07-27 10:06:43 +0300

    Remove unused logic from router

--- Sleeper/Sleeper/Application Logic/AppCoordinator/BaseCoordinator.swift
+++ Sleeper/Sleeper/Application Logic/AppCoordinator/BaseCoordinator.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 ArkadeGames. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 final class BaseCoordinator {
     
--- Sleeper/Sleeper/Application Logic/AppCoordinator/MainRouter.swift
+++ Sleeper/Sleeper/Application Logic/AppCoordinator/MainRouter.swift
@@ -8,69 +8,12 @@
 
 import UIKit
 
-class MainRouter: NSObject {
+class MainRouter {
     
-    // This stuck of navigations is all previous, when we present new navigation with viewController
-    private var previousNavigationControllers: [RSNavigationController]
-    private var currentNavigationController: RSNavigationController
+    let navigationController: RSNavigationController
     
     init(navigationController: RSNavigationController) {
-        self.currentNavigationController = navigationController
-        self.previousNavigationControllers = []
+        self.navigationController = navigationController
     }
-    
-    func push(_ viewController: UIViewController, animated: Bool) {
-        currentNavigationController.pushViewController(viewController, animated: animated)
-    }
-    
-    func present(_ viewController: UIViewController,
-                 animated: Bool,
-                 modalTransitionStyle: UIModalTransitionStyle = .coverVertical,
-                 modalPresentationStyle: UIModalPresentationStyle = .overFullScreen,
-                 completion: (() -> Void)? = nil) {
-        
-        let newNavigation = RSNavigationController(rootViewController: viewController)
-        newNavigation.modalTransitionStyle = modalTransitionStyle
-        newNavigation.modalPresentationStyle = modalPresentationStyle
-        
-        // now new navigation will present and push viewController's
-        // it will be removed, when we dismiss it
-        previousNavigationControllers.append(currentNavigationController)
-        currentNavigationController = newNavigation
-        
-        let context = topViewController()
-        context?.present(newNavigation, animated: animated, completion: completion)
-    }
-    
-    func dismissViewController(animated: Bool, completion: (() -> Void)?) {
-        let context = topViewController()
-        context?.dismiss(animated: animated, completion: completion)
-        
-        // here we take last navigation, which was before we present new
-        // and after, we remove last navigation and starting navigating from last
-        currentNavigationController = previousNavigationControllers.last!
-        previousNavigationControllers.removeLast()
-    }
-    
-}
-
-extension MainRouter {
-    func topViewController() -> UIViewController? {
-        guard let window = UIApplication.shared.keyWindow, let rootViewController = window.rootViewController else {
-            return nil
-        }
-        var top: UIViewController? = rootViewController
-        while true {
-            if let presented = top?.presentedViewController {
-                top = presented
-            } else if let nav = top as? UINavigationController {
-                top = nav.visibleViewController
-            } else if let tab = top as? UITabBarController {
-                top = tab.selectedViewController
-            } else {
-                break
-            }
-        }
-        return top
-    }
+       
 }
